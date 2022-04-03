46f8b968caf71c7c0d36aae45fc868fd53eae892
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-04-03 16:58:11 -0500

    submitted Opposite Number 8kyu solution

---
+++ CK820220403.js
@@ -0,0 +1,16 @@
+/*
+Opposite number
+
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+  return number === number ? number * (-1) : number * number
+}
