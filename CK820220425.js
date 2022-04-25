43ae06472dc6ed6af69855b37179338b803f2f56
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-04-25 14:18:09 -0500

    submitted Abbreviate a Two Word Name 8kyu solution

--- CK820220425.js
+++ CK820220425.js
@@ -12,6 +12,20 @@ Sam Harris => S.H
 patrick feeney => P.F
 */
 
+function abbreviateName(name) {
+   return (
+     name
+       // split the name into two words
+       .split(" ")
+ 
+       // take first char of both parts and capitalize them
+       .map((part) => part[0].toUpperCase())
+ 
+       // join both characters with a dot
+       .join(".")
+   );
+ }
+
 function abbrevName(name){
     const array = name.split(" ")
     return (array[0][0] + "." + array[1][0]).toUpperCase()
