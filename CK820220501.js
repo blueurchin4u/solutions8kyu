b701fb9811a5ed8e4ce520ed21a3aad3d9d9d4bb
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-05-01 21:25:22 -0500

    submitted Remove exclamation marks 8kyu

--- CK820220501.js
+++ CK820220501.js
@@ -3,4 +3,5 @@ Remove exclamation marks
 
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 */
+
 const removeExclamationMarks = s => s.replace(/!/g, '')
