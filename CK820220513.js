522b538c5017d56a2c2506ac8d3fb0b994525ee9
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-05-13 20:28:33 -0500

    submitted How good are you really? 8kyu solution

--- CK820220513.js
+++ CK820220513.js
@@ -1,5 +1,5 @@
 /*
-How good are you really?
+How good are you really? 
 
 There was a test in your class and you passed it. Congratulations!
 But you're an ambitious person. You want to know if you're better than the average student in your class.
