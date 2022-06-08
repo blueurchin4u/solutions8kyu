65c80e04b3d75f4f58f6dcb61f8cd995fb518091
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-06-07 20:25:22 -0500

    submitted Calculate average 8kyu

---
+++ CK820220607.js
@@ -0,0 +1,14 @@
+/*
+Calculate average
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+const find_average = array => { 
+    const average = array.reduce((previous, current) => previous + current, 0)/array.length
+    return array.length === 0 ? 0 : average
+    }
+  
+  
