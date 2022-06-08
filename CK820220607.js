6e9f325ff3736cc93189bc62b9a6628ec8125cbd
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-06-07 20:41:33 -0500

    submitted Calculate average 8kyu solution

--- CK820220607.js
+++ CK820220607.js
@@ -8,7 +8,7 @@ Note: Empty arrays should return 0.
 
 const find_average = array => { 
     const average = array.reduce((previous, current) => previous + current, 0)/array.length
-    return array.length === 0 ? 0 : average
+    return array.length === 0 ? 0 : average 
     }
   
   
