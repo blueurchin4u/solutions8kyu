a28cc065c1b1ff6e6544a56df765cebd02db1ccc
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-06-17 20:52:13 -0500

    submitted Grasshopper - Grade book 8kyu solution

--- CK820220617.js
+++ CK820220617.js
@@ -19,7 +19,7 @@ const getGrade = (s1, s2, s3) => {
     return average >= 90 && average <= 100 ? 'A' :
            average >= 80 && average < 90 ? 'B' :
            average >= 70 && average < 80 ? 'C' :
-           average >= 60 && average < 70? 'D' : 'F'
+           average >= 60 && average < 70? 'D' : 'F' 
          }
   
   
