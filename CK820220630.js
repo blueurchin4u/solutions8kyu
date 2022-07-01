f3c91a84f12db54bb679160a5c41ff7ea9385a56
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-06-30 20:57:22 -0500

    submitted If you can't sleep, just count sheepgit add . 8kyu solution

--- CK820220630.js
+++ CK820220630.js
@@ -12,7 +12,7 @@ var countSheep = function (num) {
     let sheep = ''
 
     for (i = 1; i <= num; i++) {
-        sheep += i + ' sheep...'
+        sheep += i + ' sheep...' 
     } 
     return sheep 
 }
