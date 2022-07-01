7508e2fe00cde0ccd7bf2c101f42c4441825a480
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-06-30 22:08:24 -0500

    submitted If you can't sleep, just count sheepgit add . 8kyu

---
+++ CK820220630.js
@@ -0,0 +1,18 @@
+/*
+If you can't sleep, just count sheep!!
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+1.  create sheep variable
+2.  loop through array starting at 1(i)
+3.  add  that to sheep variable plus added murmur
+*/
+
+var countSheep = function (num) {
+    let sheep = ''
+
+    for (i = 1; i <= num; i++) {
+        sheep += i + ' sheep...'
+    }
+    return sheep
+}
