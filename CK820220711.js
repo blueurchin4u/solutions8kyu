83cdb7fa845d6ab042fb02cac528997b91f40bac
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-07-11 20:40:41 -0500

    submitted Transportation on vacation 8kyu solution

--- CK820220711.js
+++ CK820220711.js
@@ -8,9 +8,9 @@ You will need a rental car in order for you to get around in your vacation. The 
 Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 Write a code that gives out the total amount for different days(d).
-*/
+*/ 
 
-const rentalCarCost = d => {
+const rentalCarCost = d => { 
     const total = d * 40
     d >= 7 ? total - 50 : d >= 3 ? total - 20 : total 
   }
