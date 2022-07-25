452475ff0a06a610d3945dfe33bc43edd1631aef
blueurchin4u <93684449+blueurchin4u@users.noreply.github.com>
2022-07-25 14:35:30 -0500

    submitted 8kyu solution test

---
+++ CK820220725.js
@@ -0,0 +1,27 @@
+/*
+Century From Year
+
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+1.  If the remainder of year strictly equals 0, then return 0 - if not, return 1 and add that to the return statement.
+2.  
+*/
+
+const century = year => {
+
+    let calc = (year % 100 === 0) ? 0 : 1;
+    let century = Math.floor(year / 100);
+
+    return calc + century;
+}
+
